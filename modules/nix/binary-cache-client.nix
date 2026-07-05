1a56f4fdcc021cb293b6ffa561b1a395ef5da61f
0x2B <nhagan345@gmail.com>
2026-07-05 13:42:40 -0400

    Added timeout on binary cache client module

--- modules/nix/binary-cache-client.nix
+++ modules/nix/binary-cache-client.nix
@@ -7,6 +7,9 @@
             trusted-public-keys = [
                 "yorha-cache-1:cGZJhVcbNhxMs6Fvc6YPVXiMB7BuAxhoj5xGZnoQ76A="
             ];
+
+            connect-timeout = 2;
+            download-attempts = 1;
         };
     };
 }
