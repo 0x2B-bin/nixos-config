bcbf4f689619c0660e488f5ae420209c5d6e5040
0x2B <nhagan345@gmail.com>
2026-08-22 14:37:49 -0400

    Networking changes

--- modules/profiles/common.nix
+++ modules/profiles/common.nix
@@ -19,5 +19,11 @@
       precedence ::ffff:0:0/96 100
     '';
     networking.enableIPv6 = false;
+
+    boot.kernel.sysctl = {
+      "net.ipv6.conf.all.disable_ipv6" = 1;
+      "net.ipv6.conf.default.disable_ipv6" = 1;
+      "net.ipv6.conf.enp7s0.disable_ipv6" = 1;
+    };
   };
 }
